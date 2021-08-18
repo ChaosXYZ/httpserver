6706fff16356d08012622201faf460be09493a9e
Chaos <chaoscodepractice@gmail.com>
2021-08-19 00:23:37 +0100

    Parsing 2

--- src/http/request.rs
+++ src/http/request.rs
@@ -20,7 +20,7 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
 
         if protocol != "HTTP/1.1" {
@@ -30,7 +30,11 @@ impl TryFrom<&[u8]> for Request {
         let method: Method= method.parse()?;
 
         let mut query_string = None;
-        path.find('?')
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i]; 
+        }
 
         unimplemented!();
     }
