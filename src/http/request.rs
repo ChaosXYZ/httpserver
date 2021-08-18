77a797f69d21687eec35ad4446d7768a59e3f959
Chaos <chaoscodepractice@gmail.com>
2021-08-18 20:40:01 +0100

    Parsing

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,6 +6,7 @@ use std::fmt::Result as fmtresult;
 use std::fmt::Formatter;
 use std::str;
 use std::str::Utf8Error;
+use super::method::MethodError;
 
 pub struct Request {
     path: String,
@@ -18,15 +19,45 @@ impl TryFrom<&[u8]> for Request {
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequestError)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocolError);
+        }
+
+        let method: Method= method.parse()?;
+
+        let mut query_string = None;
+        path.find('?')
+
         unimplemented!();
     }
 
 
 }
 
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+
+    for (i,c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            Some((&request[..i],&request[i+1..]));
+        }
+    }
+
+    None
+}
+
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncodingError
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidMethodError
     }
 }
 
