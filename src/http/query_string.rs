a4f587ea514353da4a6f0ec7395ec05900b0e9e4
Chaos <chaoscodepractice@gmail.com>
2021-08-21 02:34:34 +0100

    Working on query strings

---
+++ src/http/query_string.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+
+pub struct QueryString<'buf> {
+    data: HashMap<&'buf str, Value<'buf>>,
+
+}
+
+pub enum Value<'buf> {
+    Single(&'buf str),
+    Multiple(Vec<&'buf str>),
+}
+
+impl<'buf > QueryString<'buf> {
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+}
+
+impl<'buf> From<&'buf str> for QueryString<'buf>{
+    fn from(s: &'buf str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&'){
+            let key = sub_str;
+            let value = "";
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                value = &sub_str[i+1..];
+
+            }
+        }
+
+        Self {data}
+
+    }
+}
